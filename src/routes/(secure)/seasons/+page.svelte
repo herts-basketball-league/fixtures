1bba3e6c05b48d5e4d3e8c9cc7f01693d9213244
Richard Herbert <richard@infoweb.co.uk>
2026-03-14 10:13:47 +0000

    Added tailwind styling

--- src/routes/(secure)/seasons/+page.svelte
+++ src/routes/(secure)/seasons/+page.svelte
@@ -8,11 +8,88 @@
 		month: 'short',
 		year: 'numeric'
 	} );
-
 </script>
 
-<h1>Seasons</h1>
+<div class="px-4 sm:px-6 lg:px-8">
+	<div class="sm:flex sm:items-center">
+		<div class="sm:flex-auto">
+			<h1 class="text-base font-semibold text-gray-900 dark:text-white">Seasons</h1>
+			<p class="mt-2 text-sm text-gray-700 dark:text-gray-300">A list of all the seasons.</p>
+		</div>
 
+		<div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+			<button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500">
+				<a href="/seasons/add">Add Season</a>
+			</button>
+		</div>
+	</div>
+
+	<div class="mt-8 flow-root">
+		<div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+			<div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+				<div
+					class="overflow-hidden shadow-sm outline-1 outline-black/5 sm:rounded-lg dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+					<table class="relative min-w-full divide-y divide-gray-300 dark:divide-white/15">
+						<thead class="bg-gray-50 dark:bg-gray-800/75">
+							<tr>
+								<th scope="col" class="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-6 dark:text-gray-200">
+									Name
+								</th>
+								<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-gray-200">
+									Start Date
+								</th>
+								<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-gray-200">
+									End Date
+								</th>
+								<th scope="col" class="py-3.5 pr-4 pl-3 sm:pr-6">
+									<span class="sr-only">Edit</span>
+								</th>
+							</tr>
+						</thead>
+
+						<tbody class="divide-y divide-gray-200 bg-white dark:divide-white/10 dark:bg-gray-800/50">
+							{#each data.seasons as season}
+								<tr>
+									<td class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-6 dark:text-white">
+										<a href="/seasons/{ season.id }" class="text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300">
+											{ season.name }
+											<span class="sr-only">{ season.name }</span>
+										</a>
+
+
+
+
+									</td>
+									<td class="px-3 py-4 text-sm whitespace-nowrap text-gray-500 dark:text-gray-400">
+										{ formatDate.format( new Date( season.startDate ) ) }
+									</td>
+									<td class="px-3 py-4 text-sm whitespace-nowrap text-gray-500 dark:text-gray-400">
+										{ formatDate.format( new Date( season.endDate ) ) }
+									</td>
+									<td class="py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-6">
+										<form method="POST" action="?/delete" class="text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300" use:enhance={ ( { cancel } ) => {
+											if ( !confirm( `Are you sure you want to delete "${season.name}"?` ) ) {
+												cancel();
+												return;
+											}
+										} }>
+											<input name="id" type="hidden" value={season.id} />
+											<button type="submit" class="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500">Delete</button>
+											<span class="sr-only">{ season.name }</span>
+										</form>
+									</td>
+								</tr>
+							{/each}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<!--
 <p><a href="seasons/add" class="button">Add Season</a></p>
 
 <figure>
@@ -49,4 +126,4 @@
 			{/each}
 		</tbody>
 	</table>
-</figure>
+</figure> -->
--- src/routes/(secure)/seasons/[id]/+page.svelte
+++ src/routes/(secure)/seasons/[id]/+page.svelte
@@ -2,34 +2,52 @@
 	import type { PageData, ActionData } from './$types';
 
 	let { data, form }: { data: PageData, form: ActionData } = $props();
-
-	// let data.season = data.season;
 </script>
 
-<h1>{ data.season ? 'Update' : 'Add' } Season</h1>
-
-{#if form?.error}
-	<p class="error">{ form.error }</p>
-{/if}
-
 <form method="POST">
-	<p>
-		<label for="name">Name *</label>
-		<input id="name" name="name" type="text" value={ data.season?.name || '' } required/>
-	</p>
+	<div class="space-y-12">
+		<div class="border-gray-900/10 pb-12 dark:border-white/10">
+			<h2 class="text-base/7 font-semibold text-gray-900 dark:text-white">{ data.season ? 'Update' : 'Add' } Season</h2>
 
-	<p>
-		<label for="startDate">Start date *</label>
-		<input id="startDate" name="startDate" type="date" value={ data.season?.startDate || '' } required/>
-	</p>
+			<div class="col-span-full">
+				<label for="name" class="block text-sm/6 font-medium text-gray-900 dark:text-white">
+					Season name *
+				</label>
 
-	<p>
-		<label for="endDate">End date</label>
-		<input id="endDate" name="endDate" type="date" value={ data.season?.endDate || '' }/>
-	</p>
+				<div class="mt-2">
+					<input id="name" type="text" name="name" autocomplete="season-name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" maxlength="255" value={ data.season?.name || '' } required />
+				</div>
+			</div>
 
-	<p>
-		<button>{ data.season ? 'Update' : 'Add' } season</button>
-		<a href="/seasons">cancel</a>
-	</p>
+			<div class="border-gray-900/10 pb-12 dark:border-white/10">
+				<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+					<div class="sm:col-span-3">
+						<label for="startDate" class="block text-sm/6 font-medium text-gray-900 dark:text-white">
+							Start date *
+						</label>
+
+						<div class="mt-2">
+							<input id="startDate" type="date" name="startDate" autocomplete="start-date" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" value={ data.season?.startDate || '' } required />
+						</div>
+					</div>
+
+					<div class="sm:col-span-3">
+						<label for="endDate" class="block text-sm/6 font-medium text-gray-900 dark:text-white">
+							End date
+						</label>
+
+						<div class="mt-2">
+							<input id="endDate" type="date" name="endDate" autocomplete="end-date" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" value={ data.season?.endDate || '' } />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="mt-6 flex items-center justify-start gap-x-6">
+		<button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:focus-visible:outline-indigo-500">{ data.season ? 'Update' : 'Add' } Season</button>
+
+		<button type="button" class="text-sm/6 font-semibold text-gray-900 dark:text-white"><a href="/seasons">cancel</a></button>
+	</div>
 </form>
