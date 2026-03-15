08c17fc2f510d840dc9e6263be874b2828e9d06f
Richard Herbert <richard@infoweb.co.uk>
2026-03-15 09:15:44 +0000

    Removed redundant policy

--- src/lib/server/db/supabase/policies.sql
+++
@@ -1,6 +0,0 @@
--- Enable RLS and all access is denied by default
-ALTER TABLE seasons ENABLE ROW LEVEL SECURITY;
-
--- Public read
-CREATE POLICY "public can read seasons"
-ON seasons FOR SELECT USING (true);
