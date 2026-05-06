73f37c0ed4b35aec9067f3089797a36883fc5d85
Richard Herbert <richard@infoweb.co.uk>
2026-05-06 20:43:46 +0100

    Updated default page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Hertfordshire Basketball League</h1>
+<h2>Fixture Scheduler</h2>
